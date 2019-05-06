92625e0a35829b7e04098831ed3e6a9a578c5a5a
Taehyun Lim <dlatns0201@naver.com>
2019-05-06 21:51:53 +0900

    지도 불러오기 완료

--- routes/index.js
+++ routes/index.js
@@ -2,45 +2,21 @@ var express = require('express');
 var session=require("express-session");
 var fs=require("fs");
 var router = express.Router();
-// var passport=require("passport");
-// var fedInfo=require("../secret").federation;
-// var kakaoStrategy=require("passport-kakao").Strategy;
-
-// router.get("/auth/login/kakao",passport.authenticate("login-kakao"));
-// passport.use("login-kakao",new kakaoStrategy({
-//   clientID:fedInfo.kakao.client_id,
-//   callbackURL: fedInfo.kakao.callback_url
-// },
-// function(accessToken, refreshToken, profile, done){
-//   console.log("Profile: ",profile);
-//   return done(null,profile);
-// }));
-
-// router.get("/auth/login/kakao/callback",
-// passport.authenticate("login-kakao",{
-//   successRedirect:"/",
-//   failureRedirect:"/login"
-// }));
 
 /* GET home page. */
 router.get("/",(req,res)=>{
   res.send("hello world");
-})
-router.post("/loadMap",(req,res)=>{
-  const image= fs.readFileSync("public/images/highTech_1st.jpg");
-  const posX=req.body.posX;
-  const posY=req.body.posY;
-  // const obj={
-  //   "image":image,
-  //   "pos_x":req.body.posX,
-  //   "pos_y":req.body.posY
-  // };
-  console.log("test:",image,posX,posY);
-  res.send({
-    "map_image":image,
-    "posX":posX,
-    "posY":posY
-  });
-})
+});
+
+router.post("/post/loadMap",(req,res)=>{
+  const receive_body=req.body;
+  console.log("body:",receive_body);
+  const obj={
+    "path":"http://13.209.43.170:8000/images/"+receive_body.imgName,
+    "posX": receive_body.posX,
+    "posY": receive_body.posY
+  }
+  res.send(obj);
+});
 
 module.exports = router;
